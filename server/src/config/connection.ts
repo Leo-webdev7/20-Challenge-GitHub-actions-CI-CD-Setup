5bf7d8e884668c9411ae179858f3d48a0c1d9501
Leo <lshapirov2@gmail.com>
2024-12-10 19:09:00 -0600

    connection.ts file fixed

--- server/src/config/connection.ts
+++ server/src/config/connection.ts
@@ -3,6 +3,6 @@ dotenv.config();
 
 import mongoose from 'mongoose';
 
-mongoose.connect(process.env.MONGODB_URI || 'QuizTest');
+mongoose.connect(process.env.MONGODB_URI || '');
 
 export default mongoose.connection;
